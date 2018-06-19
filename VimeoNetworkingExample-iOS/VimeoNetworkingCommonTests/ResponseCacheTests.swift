9c71ff8a8e7d710b829cbdd0aed45211eff0a057
jasonhawkins <hellojason@me.com>
2018-06-19 19:02:52 -0400

    Remove most ResponseCacheTests

--- VimeoNetworkingExample-iOS/VimeoNetworkingCommonTests/ResponseCacheTests.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingCommonTests/ResponseCacheTests.swift
@@ -37,94 +37,13 @@ class ResponseCacheTests: XCTestCase
         
         responseCache.clear()
     }
-    
-    func test_ResponseCache_CanStoreAndRetrieveResponse()
-    {
-        let request = Request<VIMCategory>(path: "/test/path")
-        let categoryJSONDict = ResponseUtilities.loadResponse(from: "categories-animation-response.json")
-        
-        self.responseCache.setResponse(responseDictionary: categoryJSONDict!, forRequest: request)
-        
-        self.responseCache.response(forRequest: request) { result in
-            switch result
-            {
-            case .success(let responseDictionary):
-                XCTAssertNotNil(responseDictionary)
-                
-            case .failure(let error):
-                XCTFail("\(error.localizedDescription)")
-            }
-        }
-    }
-    
-    func test_ResponseCache_CanRemoveResponseFromCache()
-    {
-        let request1 = Request<VIMCategory>(path: "/test/path1")
-        let request2 = Request<VIMCategory>(path: "/test/path2")
-        
-        let categoryJSONDict = ResponseUtilities.loadResponse(from: "categories-animation-response.json")
-        
-        self.responseCache.setResponse(responseDictionary: categoryJSONDict!, forRequest: request1)
-        self.responseCache.setResponse(responseDictionary: categoryJSONDict!, forRequest: request2)
-        
-        self.responseCache.removeResponse(forKey: request1.cacheKey)
-        
-        self.responseCache.response(forRequest: request1) { result in
-            switch result
-            {
-            case .success(let responseDictionary):
-                XCTAssertNil(responseDictionary)
-                
-            case .failure(let error):
-                XCTFail("\(error.localizedDescription)")
-            }
-        }
-        
-        self.responseCache.response(forRequest: request2) { result in
-            switch result
-            {
-            case .success(let responseDictionary):
-                XCTAssertNotNil(responseDictionary)
-                
-            case .failure(let error):
-                XCTFail("\(error.localizedDescription)")
-            }
-        }
-    }
  
-    func test_ResponseCache_clearRemovesAllEntries()
-    {
-        let request1 = Request<VIMCategory>(path: "/test/path1")
-        let request2 = Request<VIMCategory>(path: "/test/path2")
-        
-        let categoryJSONDict = ResponseUtilities.loadResponse(from: "categories-animation-response.json")
-        
-        self.responseCache.setResponse(responseDictionary: categoryJSONDict!, forRequest: request1)
-        self.responseCache.setResponse(responseDictionary: categoryJSONDict!, forRequest: request2)
-        
+    func test_clear_removesAllEntries()
+    {        
+        // Try adding some data to disk.
+        // Verify that data exists on disk.
+        // Run self.responseCache.clear().
         self.responseCache.clear()
-        
-        self.responseCache.response(forRequest: request1) { result in
-            switch result
-            {
-            case .success(let responseDictionary):
-                XCTAssertNil(responseDictionary)
-                
-            case .failure(let error):
-                XCTFail("\(error.localizedDescription)")
-            }
-        }
-        
-        self.responseCache.response(forRequest: request2) { result in
-            switch result
-            {
-            case .success(let responseDictionary):
-                XCTAssertNil(responseDictionary)
-                
-            case .failure(let error):
-                XCTFail("\(error.localizedDescription)")
-            }
-        }
+        // Verify that data no longer exists on disk.
     }
-
 }
