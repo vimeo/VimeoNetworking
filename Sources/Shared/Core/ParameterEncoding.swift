5689cb25105bf387de709dc362bff185b5060911
Rogerio de Paula Assis <rogerio@vimeo.com>
2019-09-04 17:23:03 -0400

    Project fixes, removed duplicated protocol declaration

--- Sources/Shared/Core/ParameterEncoding.swift
+++ Sources/Shared/Core/ParameterEncoding.swift
@@ -8,18 +8,6 @@
 
 import Foundation
 
-// TODO: Remove this protocol in favour of the original
-// declaration that will be merged in a future PR. [RDPA 09/02/2019]
-protocol URLRequestConvertible {
-    func asURLRequest() throws -> URLRequest
-}
-
-extension URLRequest: URLRequestConvertible {
-    func asURLRequest() throws -> URLRequest {
-        return self
-    }
-}
-
 /// The type that describes how parameters are used in a `URLRequest`.
 protocol ParameterEncoding {
     func encode(_ requestConvertible: URLRequestConvertible, with parameters: Any?) throws -> URLRequest
